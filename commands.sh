e68a776f29175607db85e90bb17166b19826e965
Maxim Yefremov <maxYefr@gmail.com>
2023-03-09 00:37:19 +0600

    client connects to server

---
+++ commands.sh
@@ -0,0 +1,11 @@
+set -ex
+
+server(){
+  go run server.go
+}
+
+client(){
+  go run client.go
+}
+
+"$@"
--- server.go
+++ server.go
@@ -3,11 +3,55 @@ package main
 import (
 	"github.com/pkg/errors"
 	"log"
+	"net"
+	"os"
+	"time"
 
 	hc "github.com/catalinc/hashcash"
 )
 
+const (
+	HOST = "localhost"
+	PORT = "9001"
+	TYPE = "tcp"
+)
+
 func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	listen, err := net.Listen(TYPE, HOST+":"+PORT)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+	log.Printf("listening %+v %+v:%+v ...", TYPE, HOST, PORT)
+	// close listener
+	defer listen.Close()
+	for {
+		conn, err := listen.Accept()
+		if err != nil {
+			log.Fatal(err)
+			os.Exit(1)
+		}
+		go handleIncomingRequest(conn)
+	}
+}
+func handleIncomingRequest(conn net.Conn) {
+	// store incoming data
+	buffer := make([]byte, 1024)
+	_, err := conn.Read(buffer)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// respond
+	time := time.Now().Format("Monday, 02-Jan-06 15:04:05 MST")
+	conn.Write([]byte("Hi back!\n"))
+	conn.Write([]byte(time))
+
+	// close conn
+	conn.Close()
+}
+
+func main2() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	if err := func() error {
 		h := hc.NewStd() // or .New(bits, saltLength, extra)
