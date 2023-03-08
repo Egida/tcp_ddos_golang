d36fb1e93be3561ba7ef9dca8b36bce2010998c4
Maxim Yefremov <maxYefr@gmail.com>
2023-03-09 00:27:03 +0600

    more print

--- main.go
+++ main.go
@@ -13,18 +13,33 @@ func main() {
 		h := hc.NewStd() // or .New(bits, saltLength, extra)
 
 		// Mint a new stamp
-		stamp, err := h.Mint("something")
+		stamp1, err := h.Mint("something")
 		if err != nil {
 			return errors.WithStack(err)
 		}
-		log.Printf("stamp: %+v", stamp)
-
+		stamp2, err := h.Mint("something")
+		if err != nil {
+			return errors.WithStack(err)
+		}
+		stamp7, err := h.Mint("something7")
+		if err != nil {
+			return errors.WithStack(err)
+		}
+		log.Printf("stamp1: %+v", stamp1)
+		log.Printf("stamp2: %+v", stamp2)
+		log.Printf("stamp7: %+v", stamp7)
 		// Check a stamp
 		valid := h.Check("1:20:161203:something::+YO19qNZKRs=:a31a2")
 		if valid {
 			log.Println("Valid")
 		} else {
 			log.Println("Invalid")
+		}
+		validNoDate := h.CheckNoDate("1:20:161203:something::+YO19qNZKRs=:a31a2")
+		if validNoDate {
+			log.Println("validNoDate: Valid")
+		} else {
+			log.Println("validNoDate: Invalid")
 		}
 		return nil
 	}(); err != nil {
